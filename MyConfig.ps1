f0fad340d7cbd4b03ec97989b039edd5745ab164
Dalmiro <dalmiro.granias@gmail.com>
2016-03-26 04:32:51 -0300

    first commit :)

---
+++ MyConfig.ps1
@@ -0,0 +1,34 @@
+﻿#requires -version 4.0
+
+
+
+Configuration Myconfig{
+    param(
+        [string]$computername,
+        [string[]]$vmnames,
+        [string]$VHDFilePath
+    )    
+
+    Node $computername{
+        File VHDIsPresent{
+            Ensure = 'Present'
+            DestinationPath = $VHDFilePath          
+        }            
+        
+        foreach ($VMName in $VMNames){
+
+        File "VHDCopy_$VMName"{
+            DependsOn = "[File]VHDIsPresent"
+            DestinationPath = (Join-Path "C:\VM\" ((Get-Item $VHDFilePath).BaseName + "_$VMName" + ".vhd"))
+            SourcePath = $VHDFilePath
+            Ensure = 'Present'
+            Type = 'File'            
+        }        
+        
+        }        
+    }
+}
+
+myconfig -VHDFilePath "C:\VM\Base\Nano-TP4.vhd" -vmnames 1 -computername $env:COMPUTERNAME -OutputPath C:\Github\DSCResources\MYConfig
+
+Start-DscConfiguration C:\github\DSCResources\MyConfig -Force -Wait
